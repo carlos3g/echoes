f59cbffd7f691b85d4d193895d24926ecf48bd2a
Carlos Mesquita <52337966+carlos3g@users.noreply.github.com>
2025-03-15 22:57:48 -0300

    chore: upgrade nodemailer

---
+++ packages/api/src/email/adapters/mjml.adapter.ts
@@ -0,0 +1,43 @@
+// see: https://github.com/nest-modules/mailer/blob/main/lib/adapters/mjml.adapter.ts#L8
+
+import { TemplateAdapterConfig } from '../interfaces/template-adapter-config.interface';
+import { PugAdapter } from './pug.adapter';
+import { TemplateAdapter } from '../interfaces/template-adapter.interface';
+import { MailerOptions } from '../interfaces/mailer-options.interface';
+// see: https://github.com/nest-modules/mailer/issues/982
+import mjml2html = require('mjml');
+
+export class MjmlAdapter implements TemplateAdapter {
+  private engine: TemplateAdapter | null;
+
+  constructor(
+    engine: TemplateAdapter | '' | 'pug' | 'handlebars' | 'ejs',
+    config?: TemplateAdapterConfig,
+    others?: {
+      handlebar?: {
+        helper?: any;
+      };
+    }
+  ) {
+    this.engine = engine as TemplateAdapter;
+
+    if (typeof engine == 'string') {
+      if (engine === 'pug') {
+        this.engine = new PugAdapter(config);
+      } else if (engine === '') {
+        engine = '';
+      }
+    }
+  }
+
+  public compile(mail: any, callback: any, mailerOptions: MailerOptions): void {
+    this?.engine?.compile(
+      mail,
+      () => {
+        mail.data.html = mjml2html(mail.data.html).html;
+        callback();
+      },
+      mailerOptions
+    );
+  }
+}
--- packages/api/src/email/email.module.ts
+++ packages/api/src/email/email.module.ts
@@ -1,8 +1,8 @@
+import { MjmlAdapter } from '@app/email/adapters/mjml.adapter';
 import { EmailServiceContract } from '@app/email/contracts/email-service.contract';
 import { EmailService } from '@app/email/services/email-service.service';
 import type { EnvVariables } from '@app/shared/types';
 import { MailerModule } from '@nestjs-modules/mailer';
-import { MjmlAdapter } from '@nestjs-modules/mailer/dist/adapters/mjml.adapter';
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as path from 'path';
